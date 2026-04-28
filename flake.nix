1332b60aac4e57706bbff27a41203acc66ca6ba8
Lucas <lluc23@hotmail.com>
2026-04-28 23:03:47 +0200

    change the wording in the texts
    Signed-off-by: Lucas <lluc23@hotmail.com>

--- flake.nix
+++ flake.nix
@@ -4,20 +4,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    sce.url = "github:crocoder-dev/shared-context-engineering";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      sce,
+      ...
+    }:
     flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-      in {
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            sce.packages.${system}.default
             nodejs_22
             nodePackages.pnpm
           ];
